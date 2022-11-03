23db06c039cb29242fe7687cb8624c76c1b060bc
Zerat1l <g.nenchev89@gmail.com>
2022-11-03 14:03:12 +0200

    songs complete

--- objects-songs.js
+++ objects-songs.js
@@ -10,12 +10,24 @@ function songs(arr) {
     let songs = [];
     let numberOfSongs = arr.shift();
     let typeSong = arr.pop();
-    console.log(typeSong);
+    
+    for(let i = 0; i < numberOfSongs;i++) {
+        let [songType, songName, songTime] = arr[i].split('_');
+        let song = new Song(songType, songName, songTime);
+        songs.push(song);
+    }
+
+    if(typeSong === 'all') {
+        songs.forEach((i) => console.log(i.name))
+    } else {
+        let filtered = songs.filter((i) => i.type === typeSong);
+        filtered.forEach((i) => console.log(i.name));
+    }
 
 }
 
-songs([3,
-    'favourite_DownTown_3:14',
-    'favourite_Kiss_4:16',
-    'favourite_Smooth Criminal_4:01',
-    'favourite']);
+songs([2,
+    'like_Replay_3:15',
+    'ban_Photoshop_3:48',
+    'all']
+    )
