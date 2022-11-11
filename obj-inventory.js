56af81c41b6861efcb13baf5f69ff8dabde54d57
Georgi Nenchev <39854716+Zerat1l@users.noreply.github.com>
2022-11-11 16:11:38 +0200

    Update obj-inventory.js

--- obj-inventory.js
+++ obj-inventory.js
@@ -1,5 +1,25 @@
 function inventory(arr) {
 
+    let heros = [];
+
+    arr.forEach(line => {
+        let [name, level, items] = line.split(' / ');
+    
+        let currentHero = {
+            name,
+            level:Number(level),
+            items
+        }
+        heros.push(currentHero);
+    });
+    heros.sort((a,b) => a.level - b.level);
+
+    heros.forEach(el => {
+        
+        console.log(`Hero: ${el.name}`);
+        console.log(`level => ${el.level}`);
+        console.log(`items => ${el.items}`);
+    }); 
 }
 
 inventory([
